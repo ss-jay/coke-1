55e41f854330931bdc4e222c7d06ad7c0f1730ee
ss-jay <102974611+ss-jay@users.noreply.github.com>
2022-04-11 11:06:52 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,35 @@
 (function () {
     setTimeout(() => {
-        loadPageContent("homepage")
+      console.log("This is custom.js file");
+      window.addEventListener('message', function (eventData) {
+        console.log("CHECKING FOR EVENT : Custom");
+        console.log(eventData);
+        try {
+            const mainEvent = JSON.parse(eventData.data);
+            console.log("1.1 Main event => ", mainEvent);
+            console.log("1.1 Main event -> event_code => ", mainEvent.event_code);
+            console.log("1.1 Main event -> event_code -> data  => ", mainEvent.data);
+            console.log("1.1.1 Main event -> event_code -> data -> code  => ", mainEvent.data.code);
+            console.log("1.1.2 Main event -> event_code -> data -> data  => ", mainEvent.data.data);
+            if (mainEvent && mainEvent.event_code == 'custom-event') {
+                console.log("In if loop");
+                let event = JSON.parse(eventData.data);
+                console.log("1.2 ", event.data.data);
+                if (mainEvent.data.code == 'all_lables') {
+                    config = mainEvent.data.data;
+                    console.log("this is config obj", config);
+                    console.log("this is config products", config.products);
+                     loadPageContent("homepage")
+                }
+                console.log("AFTER :: page is loading");
+            }
+
+        } catch (error) {
+            return;
+        }
+      }, false);
+        
+        
     }, 500);
 })();
 
@@ -101,15 +130,15 @@ function insertInnerProducts() {
                 <div class="product-card">
                     <div class="product-tumb inner">
                         <div class="innerbox">
-                            <img class="img__wrapper" src="https://cdn.yellowmessenger.com/Zswcic99hKO91633420423934.png" alt="">
+                            <img class="img__wrapper" src=${item.icon} alt="">
                         </div>
                     </div>
                     <div class="product__details inner">
                         <div class="product__text__wrapper">
                             <div class="product__text__wrapper">
-                                <p class="product__name">Test Product</p>
-                                <p class="product__quantity">Test description</p>
-                                <p class="product__price">$12</p>
+                               <p class="product__name">${item.name}</p>
+                                <p class="product__quantity">${item.description}</p>
+                                <p class="product__price">${item.price}</p>
                             </div>
                         </div>
                         <div class="product-bottom-details">
