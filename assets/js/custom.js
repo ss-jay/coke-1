6af57620abc8180a1fd09eed7430b9d1bff2cbb2
ss-jay <102974611+ss-jay@users.noreply.github.com>
2022-04-12 17:51:43 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,45 +1,6 @@
 (function () {
     setTimeout(() => {
          loadPageContent('homepage');
-      console.log("This is custom.js file");
-      window.addEventListener('message', function (eventData) {
-        console.log("CHECKING FOR EVENT : Custom");
-        console.log(eventData);
-        try {
-            const mainEvent = JSON.parse(eventData.data);
-            console.log("1.1 Main event => ", mainEvent);
-            console.log("1.1 Main event -> event_code => ", mainEvent.event_code);
-            console.log("1.1 Main event -> event_code -> data  => ", mainEvent.data);
-            console.log("1.1.1 Main event -> event_code -> data -> code  => ", mainEvent.data.code);
-            console.log("1.1.2 Main event -> event_code -> data -> data  => ", mainEvent.data.data);
-            if (mainEvent && mainEvent.event_code == 'custom-event') {
-                console.log("In if loop");
-                let event = JSON.parse(eventData.data);
-                console.log("1.2 ", event.data.data);
-                if (mainEvent.data.code == 'all_lables') {
-                    config = mainEvent.data.data;
-                    console.log("this is config obj", config);
-                    console.log("this is config products", config.products);
-                    window.config = config;
-                    let myiframe = window.frames['ymIframe'].document.getElementById('webviewId');
-                    myiframe.contentWindow.postMessage(JSON.stringify({
-                        event_code: 'hello', data: JSON.stringify({
-                            event: {
-                                code: "personalinfo",
-                                data: 'Hi there user'
-                            }
-                        })
-                    }), '*');
-                }
-                console.log("AFTER :: page is loading");
-            }
-
-        } catch (error) {
-            return;
-        }
-      }, false);
-        
-        
     }, 0);
 })();
 
