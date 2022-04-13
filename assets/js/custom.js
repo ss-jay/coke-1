72a82dcfc83b9dd065e76a851546e2d3690f433f
ss-jay <102974611+ss-jay@users.noreply.github.com>
2022-04-13 09:47:19 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -43,22 +43,6 @@
     }, 1500);
 })();
 
-
-function apiCall() {
-    $.ajax({
-        type: "GET", //rest Type
-        url: "https://app.yellow.ai/api/data/data/records/0-10?bot=x1649164306069&collection=category_images",
-        async: false,
-        contentType: "application/json",
-        headers: {
-            "x-api-key": "odkqIZ7DTSWG5QbCV0fsqp2T8T9ItKBhEWhm3apZ"
-        },
-        success: function (msg) {
-            console.log("Data => ", msg);   
-        }
-    });
-}
-
 function loadPageContent(page) {
     if(page === "homepage") {
         insertSearchBar();
