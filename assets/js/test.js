a38bc63c036fef283d54967358b806c918512817
ashish sharma <ashish.sharma@beamstacks.com>
2022-04-13 12:39:57 +0530

    no message

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,9 +1,39 @@
-let frameEle = document.getElementById("1649832296909");
-console.log("fram ele ", frameEle)
+(function injectJS() {
+    try {        
+        var iFrameHead = window.frames["ymIframe"].document.getElementsByTagName("head")[0];   
+        console.log("iFrameHead -- > ", iFrameHead);      
+        // var modularBars = document.createElement('script');
+        // modularBars.type = 'text/javascript';
+        // modularBars.src = 'https://aporve.github.io/vanCustom/addFrame.js';
+        // iFrameHead.appendChild(modularBars);
+    } catch(e) {
+        console.error("failed while inserting to iFrame", e);
+    }
+})();
 
-let message = JSON.stringify({
-    message: 'Hello from iframe',
-    date: Date.now(),
-});
 
-frameEle.contentWindow.postMessage(message, '*');
+window.addEventListener('message', function(eventData) {
+    console.log('onload---->')
+    console.log(eventData);
+    try{
+
+        console.error( 'Data----------------->>>',eventData.data);
+        if(eventData.data == 'custom-event') {
+            console.log("document.getElementById('ymIframe') --> ", document.getElementById('ymIframe'));
+        //     document.getElementById('ymIframe').contentWindow.postMessage({
+        //         event_code: 'ym-client-event',
+        //         data: {
+        //             event: {
+        //              code: "VEMO",
+        //              data: ""
+        //             }
+        //         }
+        //    }, '*');
+           return;
+        }
+    }catch(error){
+        console.log(error);
+        return;
+    }
+    
+}, false);
