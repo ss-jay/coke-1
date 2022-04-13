d109ef77febc27e075d5135ac919f5dc91cf20c4
ashish sharma <ashish.sharma@beamstacks.com>
2022-04-13 15:06:54 +0530

    no message

--- assets/js/test2.js
+++ assets/js/test2.js
@@ -13,4 +13,6 @@ window.addEventListener('message', function (e) {
     console.log("local test 2 file called --> ", e)
     console.log("local testing 2 ashish case 2 ", e.data);
     console.log("======= > ", window);
+
+    console.log("======= > ", document);
 });
