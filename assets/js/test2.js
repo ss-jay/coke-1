c789e7989cf6e3e53061efc5dd4ae632c0e8e137
ss-jay <102974611+ss-jay@users.noreply.github.com>
2022-04-13 16:02:17 +0530

    Update test2.js

--- assets/js/test2.js
+++ assets/js/test2.js
@@ -1,19 +1,26 @@
 parent.postMessage(JSON.stringify({ message: 'Hello from iframe', date: Date.now()}), "*");
 
 
-parent.addEventListener('message', function (e) {
-    console.log("test 2 file called --> ", e)
-    console.log("testing 2 ashish case 2 ", e.data);
-    console.log(window.frames);
-});
+// parent.addEventListener('message', function (e) {
+//     console.log("test 2 file called --> ", e)
+//     console.log("testing 2 ashish case 2 ", e.data);
+//     console.log(window.frames);
+// });
 
 
 
 window.addEventListener('message', function (e) {
     console.log("1.1 => ", e.data);
+    const mainEvent = JSON.parse(e.data);
+    
+    if (mainEvent && mainEvent.event_code == 'custom-event') {
+        if (mainEvent.data.code == 'all_lables') {
+            document.querySelector("iframe").contentWindow.postMessage(JSON.stringify({ message: '----- testing hitesh ---- ', date: mainEvent.data.data }), '*');
+        }
+    }
 //     console.log("======= > ", window);
 
 //     console.log("======= > ", document);
 
-    document.querySelector("iframe").contentWindow.postMessage(JSON.stringify({ message: '----- testing hitesh ---- ', date: e.data }), '*');
-});
+//     document.querySelector("iframe").contentWindow.postMessage(JSON.stringify({ message: '----- testing hitesh ---- ', date: e.data }), '*');
+});1
